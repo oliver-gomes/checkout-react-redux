1aba236bb1da45fd518e0375c92bb52dddc26766
Oliver Gomes <ogomesphoto@gmail.com>
2019-04-07 00:19:14 -0400

    estimate total add

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React, { Component } from "react";
 import SubTotal from "./components/Subtotal";
 import PickupSavings from "./components/PickupSavings";
 import TaxesFees from "./components/TaxesFees";
+import EstimatedTotal from "./components/EstimatedTotal";
 import "./App.css";
 
 class App extends Component {
   state = {
     total: 100,
     pickUpSavings: -3.85,
-    taxes: 0
+    taxes: 0,
+    estimatedTotal: 0
   };
   render() {
     return (
@@ -18,6 +20,8 @@ class App extends Component {
           <SubTotal price={this.state.total.toFixed(2)} />
           <PickupSavings price={this.state.pickUpSavings} />
           <TaxesFees taxes={this.state.taxes.toFixed(2)} />
+          <hr />
+          <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)} />
         </div>
       </div>
     );
