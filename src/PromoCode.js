493b947489b1a4fabd912ed368cf6cb0efb067bd
Oliver Gomes <ogomesphoto@gmail.com>
2019-04-07 01:12:23 -0400

    promo code finish up

--- src/PromoCode.js
+++ src/PromoCode.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import React, { Component } from "react";
+
 import {
   Button,
   Collapse,
-  Well,
+  Card,
   Form,
   Row,
   Col,
   FormGroup,
-  ControlLabel,
   FormControl
 } from "react-bootstrap";
 
@@ -28,6 +27,34 @@ export default class PromoCode extends Component {
           {this.state.open === false ? `Apply ` : `Hide`} promo code
           {this.state.open === false ? ` +` : ` -`}
         </Button>
+        <Collapse in={this.state.open}>
+          <div>
+            <Card>
+              <Row className="show-grid">
+                <Col md={12}>
+                  <Form>
+                    <FormGroup controlId="formInlineName">
+                      <h4>Promo Code</h4>
+                      <FormControl
+                        type="text"
+                        placeholder="Enter promo code"
+                        value="this.props.promoCode"
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    <Button
+                      block
+                      bsStyle="success"
+                      className="btn-round"
+                      disabled={this.props.isDisabled}
+                      onClick={this.props.giveDiscount}
+                    />
+                  </Form>
+                </Col>
+              </Row>
+            </Card>
+          </div>
+        </Collapse>
       </div>
     );
   }
